ac215a22dc72381319e10bd01666d6ba14492785
lehon25 <lehon_25@yahoo.com>
2018-03-06 20:00:31 +0700

    commit yang kedua untuk npm

---
+++ kalkulator.js
@@ -0,0 +1,16 @@
+function perkalian(a){
+    return a * a;
+}
+function pembagian(a){
+    return a / 4;
+}
+function penjumlahan(a){
+    return a + a + 5
+}
+function modulus(a){
+    return a % 10
+}
+
+
+
+module.exports = {perkalian,pembagian,penjumlahan,modulus};
---
+++ test.js
@@ -0,0 +1,6 @@
+var kalkulator = require("./kalkulator");
+let a =10;
+console.log(kalkulator.perkalian (3));
+console.log(kalkulator.pembagian(6));
+console.log(kalkulator.penjumlahan(5));
+console.log(kalkulator.modulus(3));
